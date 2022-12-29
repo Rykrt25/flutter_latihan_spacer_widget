4390efdcb857fa0ce81f39bd15305afcf50784d8
Rykrt25 <rifkyalmukmin@gmail.com>
2022-12-29 22:10:20 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Latihan spacer widget"),
+        ),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children:  <Widget>[
+              const Spacer(flex: 1,),
+              Container(
+                width: 80,
+                height: 80,
+                color: Colors.blue,
+              ),
+              const Spacer(flex: 2,),
+              Container(
+                width: 80,
+                height: 80,
+                color: Colors.yellow,
+              ),
+              const Spacer(flex: 3,),
+              Container(
+                width: 80,
+                height: 80,
+                color: Colors.red,
+              ),
+              const Spacer(flex: 2,)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
